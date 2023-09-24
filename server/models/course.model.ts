7ba2bdc3bca7cf46b546ce2797049cc345be621e
Milo Bedini <milobedini64@gmail.com>
2023-09-24 10:26:07 +0100

    course model

---
+++ server/models/course.model.ts
@@ -0,0 +1,130 @@
+import { Schema, model } from 'mongoose';
+
+type IComment = {
+  user: object;
+  comment: string;
+  commentReplies?: IComment[];
+} & Document;
+
+type IReview = {
+  user: object;
+  rating: number;
+  comment: string;
+  commentReplies: IComment[];
+} & Document;
+
+type ILink = {
+  title: string;
+  url: string;
+} & Document;
+
+type ICourseData = {
+  title: string;
+  description: string;
+  videoUrl: string;
+  videoThumbnail: object;
+  videoSection: string;
+  videoLength: number;
+  videoPlayer: string;
+  links: ILink[];
+  suggestion: string;
+  questions: IComment[];
+} & Document;
+
+type ICourse = {
+  name: string;
+  description: string;
+  price: number;
+  estimatedPrice?: number;
+  thumbnail: object;
+  tags: string;
+  level: string;
+  demoUrl: string;
+  benefits: { title: string }[];
+  prerequisites: { title: string }[];
+  reviews: IReview[];
+  courseData: ICourseData;
+  ratings?: number;
+  purchased?: number;
+} & Document;
+
+const reviewSchema = new Schema<IReview>({
+  user: Object,
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  comment: String,
+});
+
+const linkSchema = new Schema<ILink>({
+  title: String,
+  url: String,
+});
+
+const commentSchema = new Schema<IComment>({
+  user: Object,
+  comment: String,
+  commentReplies: [Object],
+});
+
+const courseDataSchema = new Schema<ICourseData>({
+  title: String,
+  description: String,
+  videoUrl: String,
+  videoThumbnail: Object,
+  videoSection: String,
+  videoLength: Number,
+  videoPlayer: String,
+  links: [linkSchema],
+  suggestion: String,
+  questions: [commentSchema],
+});
+
+const courseSchema = new Schema<ICourse>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  estimatedPrice: { type: Number },
+  thumbnail: {
+    public_id: {
+      required: true,
+      type: String,
+    },
+    url: {
+      required: true,
+      type: String,
+    },
+  },
+  tags: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    required: true,
+  },
+  demoUrl: {
+    type: String,
+    required: true,
+  },
+  benefits: [{ title: String }],
+  prerequisites: [{ title: String }],
+  reviews: [reviewSchema],
+  courseData: courseDataSchema,
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  purchased: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const CourseModel = model<ICourse>('Course', courseSchema);
+
+export default CourseModel;
